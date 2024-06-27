6749b27068bc6217c88bb56f10bf8bb927510aa7
nidhisahani <nidhinishad42@gmail.com>
2024-06-27 15:43:41 +0530

    updated commit

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ app.get("/", (req, res) => {
 
   
 
-// const port= process.env.PORT || 5000;
-const port =5000
+const port= process.env.PORT || 5000;
+// const port =5000
 
 
 app.listen(port, async () => {
